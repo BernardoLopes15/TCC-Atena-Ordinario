5f2ef0f1010f5dd657720b25c7286462ca9cdf68
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2023-09-12 18:06:24 -0300

    deixando responsivo

--- src/pages/presentation/index.jsx
+++ src/pages/presentation/index.jsx
@@ -2,9 +2,11 @@
 import { Link } from 'react-router-dom';
 
 import logoAtena from "../../assets/imgs/logo_atena.png";
+import logoEmpresa from "../../assets/imgs/logo_empresa.png";
+import arte from "../../assets/atena apresenta.png"
+
 /*
 import logonNavbar from "../../assets/imgs/logo_navbar.png";
-import logoEmpresa from "../../assets/imgs/logo_empresa.png";
 import perfilRian from "../../assets/imgs/perfil_rian.png";
 import perfilAna from "../../assets/imgs/perfil_ana.png";
 import perfilBernardo from "../../assets/imgs/perfil_bernardo.png";
@@ -33,14 +35,14 @@ const Apresentacao = () => {
         <main>
           <article>
             <section className="min-h-screen px-4 flex items-center justify-center inicio">
-              <div className="flex justify-between md:w-2/3">
-                <div className='text'>
-                  <img src={logoAtena} alt="logo" />
+              <div className="flex justify-between md:w-2/3 content-inicio">
+                <div className='enter'>
+                  <img src={logoAtena} alt="logo" className='logo'/>
                   <p className="text-white subtitle text-xl">Você é forte e será capaz de <br/> lutar e conquistar o seu espaço.</p>
-                  <Link to="/login"><button className="w-full md:w-60 py-2 my-4 text-purple-600 text-lg font-bold rounded-lg bg-white">Entrar</button></Link>
+                  <a><Link to="/login"><button className="w-full md:w-60 py-2 my-4 text-purple-600 text-lg font-bold rounded-lg bg-white btn">Entrar</button></Link></a>
                 </div>
-                <div className="hidden md:flex items-center">
-                  <img src={''} alt="logo" />
+                <div className="">
+                  <img src={arte} alt="arte" className='arte' />
                 </div>
               </div>
             </section>
@@ -64,28 +66,27 @@ const Apresentacao = () => {
                 <div className="md:max-w-2xl content">
                   <h3 className="title font-bold my-4 text-xl">Cuidar da saúde mental</h3>
                   <p className="subtitle text-lg">
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptate voluptatem autem repellendus distinctio voluptatum molestias neque nobis amet ullam reiciendis beatae porro natus rerum, doloribus quasi quia. Facilis, sit modi.
+                  Zelar pelo seu bem-estar mental deve ser uma entre suas prioridades de vida, o seu estado mental é refletido em todas as áreas da sua vida, desde seu comportamento no trabalho até seu relacionamento familiar. <br/> <br/> Assista o vídeo para compreender melhor a importância de cuidar da sua saúde mental.
                   </p>
                 </div>
                 <div className="w-full md:w-96 h-64 flex items-center justify-center text-white bg-black">video aqui</div>
               </div>
             </section>
 
-            <section className="px-4 py-8 md:py-32 flex items-center justify-center bg-purple-400">
-              <div className="md:w-2/3 flex flex-col-reverse md:flex-row justify-between py-6">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320" className='md:block hidden'><path fill="#fff" fill-opacity="1" d="M0,160L80,149.3C160,139,320,117,480,133.3C640,149,800,203,960,213.3C1120,224,1280,192,1360,176L1440,160L1440,0L1360,0C1280,0,1120,0,960,0C800,0,640,0,480,0C320,0,160,0,80,0L0,0Z"></path></svg>
+
+            <section className="px-4 py-8 md:py-32 flex items-center justify-center tcc">
+              <div className="md:w-2/3 flex flex-col-reverse md:flex-row justify-between py-6 content_tcc">
                 <div className="md:max-w-2xl">
-                  <h2 className="text-xl">Texto sobre o TCC</h2>
+                  <h2 className="text-xl">The Golden Developers</h2>
+                  <img src={logoEmpresa} alt="perfil" />
                   <p className="text-lg">
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolor odio, consequatur illum alias sed sapiente est tempore. Illo exercitationem provident ex reprehenderit architecto voluptatum debitis doloribus eaque, amet aliquid earum.
-                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequatur libero dolorum ut atque quas rerum ducimus voluptates vitae impedit maiores nemo laborum tempore, magnam debitis quis aliquam maxime reprehenderit animi!
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolor odio, consequatur illum alias sed sapiente est tempore. Illo exercitationem provident ex reprehenderit architecto voluptatum debitis doloribus eaque, amet aliquid earum.
-                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequatur libero dolorum ut atque quas rerum ducimus voluptates vitae impedit maiores nemo laborum tempore, magnam debitis quis aliquam maxime reprehenderit animi!
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolor odio, consequatur illum alias sed sapiente est tempore. Illo exercitationem provident ex reprehenderit architecto voluptatum debitis doloribus eaque, amet aliquid earum.
-                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequatur libero dolorum ut atque quas rerum ducimus voluptates vitae impedit maiores nemo laborum tempore, magnam debitis quis aliquam maxime reprehenderit animi!
+                  Zelar pelo seu bem-estar mental deve ser uma entre suas prioridades de vida, o seu estado mental é refletido em todas as áreas da sua vida, desde seu comportamento no trabalho até seu relacionamento familiar.Assista o vídeo ao lado para compreender melhor a importância de cuidar da sua saúde mental.
                   </p>
                 </div>
               </div>
             </section>
+
 
             <section className="min-h-screen px-4 flex items-center justify-center project">
               <div className="w-full md:w-2/3 flex justify-between py-6">
@@ -238,30 +239,12 @@ const Apresentacao = () => {
 
         <main>
           <article>
-            <section className="min-h-screen px-4 flex items-center justsify-center start">
-              <div className=" flex justify-around md:w-2/3">
-                <div>
-                  <img src={logoAtena} alt="logo" className='logo'/>
-                  <p className="text-white text-xl">Você é forte e será capaz de <br/> lutar e passar por essa fase.</p>
-                  <Link to="/login"><button className="w-full md:w-60 py-2 my-4 text-purple-600 text-lg font-bold rounded-lg bg-white btn">Entrar</button></Link>
-                </div>
-              </div>
-            </section>
-
+   
 
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320" id='project'>
               <path fill='rgb(164,132,232)' fill-opacity="1" d="M0,160L48,154.7C96,149,192,139,288,138.7C384,139,480,149,576,154.7C672,160,768,160,864,154.7C960,149,1056,139,1152,154.7C1248,171,1344,213,1392,234.7L1440,256L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"></path>
             </svg>
 
-            <section className="px-4 py-8 md:pb-32 flex items-center justify-center content_project"  >
-              <div className="md:w-2/3 md:flex justify-between py-6">
-                <div className="w-full md:w-96 h-64 flex items-center justify-center text-white bg-black">video aqui</div>
-                <div className="md:max-w-2xl">
-                  <h3 className=" title my-4 text-purple-400 text-xl">Nosso objetivo</h3>
-                  <p className=" text-lg">  A plataforma ATENA foi criada visando zelar <br/>a saúde mental de mulheres vítimas da violência, através de orientações feitas por profissionais da psicologia em consultas onlines e totalmente gratuitas.</p>
-                </div>
-              </div>
-            </section>
 
             <section className="px-4 py-8 md:py-32 flex items-center justify-center content_project2">
               <div className="md:w-2/3 flex flex-col-reverse md:flex-row justify-between py-6">
