98992905bcaeebaa0e762ccc9dbf463cf916c850
Rian-usr <rian.jesus5@etec.sp.gov.br>
2023-09-29 00:41:23 -0300

    tela calendário

--- src/pages/calendario/index.jsx
+++ src/pages/calendario/index.jsx
@@ -2,6 +2,8 @@ import { useEffect, useState } from "react";
 import NavBar from "../../components/NavbarPsicologo";
 import Rodape from "../../components/Rodape";
 
+import "../calendario/styles.css"
+
 const Calendario = () =>{
     const [mesEscolhido, setMesEscolhido] = useState(9);
     const [anoEscolhido, setAnoEscolhido] = useState(2023);
@@ -62,21 +64,27 @@ const Calendario = () =>{
         <>
             <NavBar />
             <div className="min-h-screen flex items-center justify-center bg-purple-100">
-                <div className="pt-16">
-                    <div className="my-4 flex justify-around">
-                        <p>{todosMeses[mesEscolhido - 1]}</p>
+                <div className="pt-16 " >
+                    <div className="my-4 flex justify-around text-xl text-fuchsia-950 font-medium ">
+                        <p>{todosMeses[mesEscolhido - 1].toUpperCase()}</p>
                         <p>{anoEscolhido}</p>
                     </div>
+
                     <div className="flex flex-wrap">
-                        <button className="mx-6" onClick={()=>mudarMes(2)}>voltar</button>
-                        <div className="grid grid-cols-7 bg-purple-500">
-                            <h2 className="p-2 lg:p-4 bg-white">dom</h2>
-                            <h2 className="p-2 lg:p-4 bg-white">seg</h2>
-                            <h2 className="p-2 lg:p-4 bg-white">ter</h2>
-                            <h2 className="p-2 lg:p-4 bg-white">qua</h2>
-                            <h2 className="p-2 lg:p-4 bg-white">qui</h2>
-                            <h2 className="p-2 lg:p-4 bg-white">sex</h2>
-                            <h2 className="p-2 lg:p-4 bg-white">sab</h2>
+                        <button className="mx-6" onClick={()=>mudarMes(2)}>
+                          <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="#281161" class="bi bi-arrow-left-circle" viewBox="0 0 16 16">
+                           <path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-4.5-.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5z"/>
+                          </svg>
+                        </button>
+
+                        <div className="grid grid-cols-7 bg-purple-500 rounded-lg">
+                            <h2 className="p-2 lg:p-4 bg-white rounded-s-md">DOM</h2>
+                            <h2 className="p-2 lg:p-4 bg-white ">SEG</h2>
+                            <h2 className="p-2 lg:p-4 bg-white">TER</h2>
+                            <h2 className="p-2 lg:p-4 bg-white">QUA</h2>
+                            <h2 className="p-2 lg:p-4 bg-white">QUI</h2>
+                            <h2 className="p-2 lg:p-4 bg-white">SEX</h2>
+                            <h2 className="p-2 lg:p-4 bg-white rounded-e-md">SAB</h2>
                             {
                                 inicioMes.map(()=>(
                                     <h2></h2>
@@ -84,11 +92,17 @@ const Calendario = () =>{
                             }
                             {
                                 dia.map((dia)=>(
-                                    <h2 className="p-4 lg:p-8 flex items-center justify-center hover:bg-purple-300 cursor-pointer text-white bg-purple-500">{dia}</h2>
+                                    <h2 className="p-4 lg:p-8 flex items-center justify-center hover:bg-purple-300 cursor-pointer text-white bg-purple-500 rounded-md">{dia}</h2>
                                 ))
                             }
                         </div>
-                        <button className="mx-6" onClick={()=>mudarMes(1)}>avancar</button>
+
+                        <button className="mx-6" onClick={()=>mudarMes(1)}>
+                         <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="#281161" class="bi bi-arrow-right-circle" viewBox="0 0 16 16">
+                          <path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z"/>
+                         </svg>
+                        </button>
+
                     </div>
                 </div>
             </div>
