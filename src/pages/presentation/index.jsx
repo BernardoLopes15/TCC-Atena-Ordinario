6bac628512de32199c124a9d02ee79cdbeb1f123
Rian-usr <rian.jesus5@etec.sp.gov.br>
2023-09-29 22:53:45 -0300

    details presentation

--- src/pages/presentation/index.jsx
+++ src/pages/presentation/index.jsx
@@ -53,21 +53,26 @@ const Apresentacao = () =>{
         </div>
       </nav>
     </header>
+
     <main>
       <article>
         <section className="min-h-screen px-4 flex items-center justify-center inicio">
           <div className="flex items-center justify-between md:w-9/12">
-            <img className='w-5/12 md:block hidden' src={iconMuler} alt="mulher icon" />
-            <div className='md:mr-48'>
+            <img className='w-5/12 md:block  hidden mr-20' src={iconMuler} alt="mulher icon" />
+
+            <div className='md:mr-48 ml-10'>
               <img src={logoAtena} alt="logo" className='w-96' />
               <p className="py-6 text-white text-center text-xl">Você é forte, será capaz de <br /> lutar e conquistar o seu espaço.</p>
-              <Link to="/login"><button className="w-96 py-2 my-4 text-purple-600 text-lg font-bold rounded-lg bg-white">Entrar</button></Link>
+              <Link to="/login"><button className="w-96 py-2 my-4 text-purple-800 text-lg font-bold rounded-lg bg-white">Entrar</button></Link>
             </div>
+
           </div>
         </section>
+
         <svg className='md:block hidden svg1' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320" >
           <path fill='rgb(164,132,232)' fill-opacity="1" d="M0,160L48,154.7C96,149,192,139,288,138.7C384,139,480,149,576,154.7C672,160,768,160,864,154.7C960,149,1056,139,1152,154.7C1248,171,1344,213,1392,234.7L1440,256L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z" id='project'></path>
         </svg>
+
         <section className="px-4 py-8 md:pb-32 flex items-center justify-center objetivo " >
           <div className="md:w-2/3 md:flex justify-between py-6 content-objetivo">
             <div className="w-full md:w-96 h-64 flex items-center justify-center text-white bg-black">video aqui</div>
@@ -77,6 +82,7 @@ const Apresentacao = () =>{
             </div>
           </div>
         </section>
+
         <section className="px-4 py-8 md:py-32 flex items-center justify-center saude">
           <div className="md:w-2/3 flex flex-col-reverse md:flex-row justify-between py-6 content-saude">
             <div className="md:max-w-2xl">
@@ -88,37 +94,40 @@ const Apresentacao = () =>{
             <div className="w-full md:w-96 h-64 flex items-center justify-center text-white bg-black">video aqui</div>
           </div>
         </section>
+
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320" className='md:block hidden svg2'>
+          <path fill="#fff" fill-opacity="1" d="M0,160L80,149.3C160,139,320,117,480,133.3C640,149,800,203,960,213.3C1120,224,1280,192,1360,176L1440,160L1440,0L1360,0C1280,0,1120,0,960,0C800,0,640,0,480,0C320,0,160,0,80,0L0,0Z"></path></svg>
+
         <section className="px-4 py-8 flex items-center justify-center text-white inicio" id='aboutUs'>
           <div className="md:w-9/12 flex flex-col-reverse md:flex-row justify-between py-6">
             <div className='w-full'>
+
               <div className='flex items-center justify-center flex-wrap'>
-                <img className='md:mr-16 md:w-96 lg:w-auto' src={logoEmpresa} alt="perfil" /> 
-                <div className='lg:w-96'>
-                  <h2 className="py-4 font-title">The Golden Developers</h2>
-                  <p className="">
+                <img className='md:mr-16 rounded-2xl img-size' src={logoEmpresa} alt="logo empresa" /> 
+
+                <div className='width-text'>
+                  <h2 className="py-4 font-title text-5xl font-medium">The Golden Developers</h2>
+                  <p className="text-xl">
                     A The Golden Developers é uma empresa com uma equipe de 5 integrantes, que mira em um projeto de importância social, a fim de construir uma sociedade mais acolhedora e segura, principalmente para o público feminino.
                     Dessa forma, visamos disponibilizar para as mulheres vítimas de situações violentas e traumáticas, a oportunidade de voltar a se sentirem confortáveis em qualquer tipo de relacionamento.
                   </p>
                 </div>
+
               </div>
               <div className='w-full'>
-                <h3 className='pt-8 text-center font-title'>Nossos valores:</h3>
+                <h3 className='pt-8 text-center font-title text-5xl'>Nossos valores:</h3>
                 <div className='flex justify-between flex-wrap py-16'>
                   <div className='w-64 my-10 lg:my-0 md:mr-4'>
-                    <h4 className='text-xl'>Prosperidade</h4>
-                    <p>Se esta é a nossa vontade, então devemos aplicá-la.</p>
+                    <h4 className='text-2xl font-medium'>Respeito</h4> 
+                    <p className='text-xl'>O mundo se tornará melhor apenas se um acordo de respeito for estabelecido entre todas as pessoas.</p>
                   </div>
                   <div className='w-64 my-10 lg:my-0 md:mr-4'>
-                    <h4 className='text-xl'>Respeito</h4> 
-                    <p>O mundo se tornará melhor apenas se um acordo de respeito for estabelecido entre todas as pessoas.</p>
-                  </div>
-                  <div className='w-64 my-10 lg:my-0 md:mr-4'>
-                    <h4 className='text-xl'>Altruísmo</h4>
-                    <p> Não devemos esperar nada em troca ao fazer o que é necessário para o mundo.</p> 
+                    <h4 className='text-2xl font-medium'>Altruísmo</h4>
+                    <p className='text-xl'>  Não devemos esperar nada em troca ao fazer o que é necessário para o mundo.</p> 
                   </div>
                 <div className='w-64 my-10 lg:my-0 md:mr-4'>
-                  <h4 className='text-xl'>Beneficiência</h4>
-                  <p>Não podemos deixar as pessoas mais vulneráveis na mão.</p> 
+                  <h4 className='text-2xl font-medium'>Beneficiência</h4>
+                  <p className='text-xl'>Não podemos deixar as pessoas mais vulneráveis na mão.</p> 
                 </div>
               </div>
             </div>
