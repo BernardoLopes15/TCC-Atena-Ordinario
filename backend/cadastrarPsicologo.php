daed310b445627fed2cf30554a3a8cddf1c47b1b
Rian-usr <rian.jesus5@etec.sp.gov.br>
2023-10-26 17:25:48 -0300

    termos de uso (arrumar posição)

--- backend/cadastrarPsicologo.php
+++ backend/cadastrarPsicologo.php
@@ -1,5 +1,11 @@
 <?php
+    // use PHPMailer\PHPMailer\PHPMailer;
+    // use PHPMailer\PHPMailer\SMTP;
+    // use PHPMailer\PHPMailer\Exception;
+
+	// require'./mailer/vendor/autoload.php';
     include("conexao.php");
+	
 
     $nomePessoa = "";
 	$cpf = "";
@@ -9,6 +15,23 @@
 	$senha = "";
 	$sql = "";
 	$bio = "";
+
+	// $mail = new PHPMailer(true);
+
+	// try {
+	// 	$mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+	// 	$mail->isSMTP();                                            //Send using SMTP
+	// 	$mail->Host       = 'localhost:3306';                     //Set the SMTP server to send through
+	// 	$mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+	// 	$mail->Username   = 'root';                     //SMTP username
+	// 	$mail->Password   = 'root';                               //SMTP password
+	// 	$mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+	// 	$mail->Port       = 3306;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`	
+
+	// } catch (Exception $e) {
+	// 	echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+
+	// }
 	
 	if($_SERVER['REQUEST_METHOD'] == 'POST') {
 		$valorRecebido = file_get_contents("php://input");
--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -97,10 +97,7 @@ const Login = () =>{
                                 {
                                     menucadastro && <div className="fixed inset-0 flex items-center justify-center square" onClick={()=>setMenuCadastro(false)}>
                                         <div className="p-4  md:bg-white type-account">
-                                        {/* <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-x-circle" viewBox="0 0 16 16" className="exit">
-                                        <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
-                                        <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
-                                        </svg> */}
+                                  
 
                                             <h2 className="text-xl font-bold text-purple-500">Contas</h2>
                                             <h2 className="text-xl">Selecione um tipo de conta:</h2>
