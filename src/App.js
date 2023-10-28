cf79bd51fc24aaddd33b02b8110f6985626f7a15
Rian-usr <rian.jesus5@etec.sp.gov.br>
2023-10-27 22:20:23 -0300

    termos de uso

--- src/App.js
+++ src/App.js
@@ -24,6 +24,7 @@ import Calendario from "./pages/calendario";
 import SolicitacaoConsulta from "./pages/solicitacaoConsulta";
 import ConsultaPsicologo from "./pages/consulta/psicologo";
 import CadastroData from "./pages/cadastrarData";
+import TermosUso from "./pages/cadastro/termoUso";
 
 const App = () =>{
   return (
@@ -33,6 +34,7 @@ const App = () =>{
         <Route path="/login" element={<Login />}/>
         <Route path="/cadastro" element={<CadastroCliente />}/>
         <Route path="/cadastroPsicologo" element={<CadastroPsicologo />}/>
+        <Route path="/termosUso" element={<TermosUso />}/>
 
         { /* Tela Cliente */}
         <Route path="/homeCliente" element={<HomeCliente />}/>
