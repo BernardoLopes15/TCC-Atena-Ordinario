c32a6ba594cc7e9978095aa3fe93668ea82a2f33
wellyngton-souza <wellyngtonteixeira12@gmail.com>
2023-11-14 13:00:28 -0300

    feat: todo-consulta

--- src/pages/cadastrarData/index.jsx
+++ src/pages/cadastrarData/index.jsx
@@ -1,78 +1,44 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 const CadastroData = () =>{
-    const dias = [
-        "domingo",
-        "segunda",
-        "terca",
-        "quarta",
-        "quinta",
-        "sexta",
-        "sabado"
-    ];
-
-    const [dia, setDia] = useState("segunda");
-    const [hrComeco, setHrComeco] = useState();
-    const [hrFim, setHrFim] = useState();
+    const [dia, setDia] = useState("0");
+    const [hrComeco, setHrComeco] = useState("10:42");
+    const [hrFim, setHrFim] = useState("20:53");
 
     const [consulta, setConsulta] = useState(
         [
             {
-                dia: "segunda",
-                dtInicio: "14:12",
-                dtTermino: "14:12"
-            },
-            {
-                dia: "segunda",
-                dtInicio: "14:12",
-                dtTermino: "14:12"
-            },
-            {
-                dia: "segunda",
-                dtInicio: "14:12",
-                dtTermino: "14:12"
-            },
-            {
-                dia: "segunda",
-                dtInicio: "14:12",
-                dtTermino: "14:12"
-            },
-            {
-                dia: "segunda",
-                dtInicio: "14:12",
-                dtTermino: "14:12"
-            },
-            {
-                dia: "segunda",
+                dia: "0",
                 dtInicio: "14:12",
                 dtTermino: "14:12"
             }
         ]
     );
 
-    const mudarDia = (diaSelect) => setDia(diaSelect);
-
-    const cadastrarData = (e) =>{
-        e.preventDefault();
-
-        setConsulta(
-            {
-                dia: dia,
-                hrComeco: hrComeco,
-                hrFim: hrFim
-            }
-        );
+    const adicionarData = () => {
+        setConsulta((prevState) => [...prevState, {dia: dia,dtInicio: hrComeco,dtTermino: hrFim}]);
+        verIntervaloData();
+    };
+    
+    const verIntervaloData = () =>{
+        for(let i = 0; i < consulta.length; i++) console.log(consulta[i]);
     }
+
+    const apagarData = (index) => {
+        let novaData = [...consulta];
+        novaData.splice(index, 1);
+        setConsulta(novaData);
+    };
 
     return(
         <main>
             <article>
                 <div className="min-h-screen flex items-center justify-center">
-                    <div className="w-full my-16 md:w-6/12 lg:w-5/12 md:p-12 rounded-lg bg-white">
+                    <div className="w-full my-16 md:w-6/12 lg:w-5/12 md:p-12 p-4 border-box rounded-lg bg-white">
                         <div className="mt-2">
                             <Link to='/calendario'>
-                                <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="#281161" class="bi bi-box-arrow-left" viewBox="0 0 16 16">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="#281161" className="bi bi-box-arrow-left" viewBox="0 0 16 16">
                                     <path fill-rule="evenodd" d="M6 12.5a.5.5 0 0 0 .5.5h8a.5.5 0 0 0 .5-.5v-9a.5.5 0 0 0-.5-.5h-8a.5.5 0 0 0-.5.5v2a.5.5 0 0 1-1 0v-2A1.5 1.5 0 0 1 6.5 2h8A1.5 1.5 0 0 1 16 3.5v9a1.5 1.5 0 0 1-1.5 1.5h-8A1.5 1.5 0 0 1 5 12.5v-2a.5.5 0 0 1 1 0v2z"/>
                                     <path fill-rule="evenodd" d="M.146 8.354a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L1.707 7.5H10.5a.5.5 0 0 1 0 1H1.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3z"/>
                                 </svg>
@@ -82,26 +48,26 @@ const CadastroData = () =>{
                                 <div className=" my-6 flex justify-between">
                                     <div>
                                         <h4 className="text-lg text-purple-700">Dia da Semana</h4>
-                                        <select name="">
-                                            <option value="">domingo</option>
-                                            <option value="">segunda</option>
-                                            <option value="">terça</option>
-                                            <option value="">quarta</option>
-                                            <option value="">quinta</option>
-                                            <option value="">sexta</option>
-                                            <option value="">sabado</option>
+                                        <select onChange={(e)=> setDia(e.target.value)}>
+                                            <option value="0">domingo</option>
+                                            <option value="1">segunda</option>
+                                            <option value="2">terça</option>
+                                            <option value="3">quarta</option>
+                                            <option value="4">quinta</option>
+                                            <option value="5">sexta</option>
+                                            <option value="6">sábado</option>
                                         </select>
                                     </div>
                                     <div>
                                         <h4 className="text-lg text-purple-700">Hora de Inicio</h4>
-                                        <input type="time" name="" id="" />
+                                        <input type="time" onChange={(e)=>setHrComeco(e.target.value)} />
                                     </div>
                                     <div>
                                         <h4 className="text-lg text-purple-700">Hora de Término</h4>
-                                        <input type="time" name="" id="" />
+                                        <input type="time" onChange={(e)=>setHrFim(e.target.value)} />
                                     </div>
                                 </div>
-                                <button>Adicionar</button>
+                                <button onClick={adicionarData}>Adicionar</button>
                                 <div className="mt-12 border grid grid-cols-5">
                                     <div className="px-4 border-box my-2">e.dia</div>
                                     <div className="my-2">e.dtInicio</div>
@@ -110,13 +76,34 @@ const CadastroData = () =>{
                                     <div className="my-2"></div>
                                 </div>
                                 <div className="border grid grid-cols-5">
-                                    {consulta.map((e)=>(
+                                    {consulta.map((e, index)=>(
                                         <>
-                                            <div className="px-4 border-box my-2">{e.dia}</div>
+                                            <div key={e.dtInicio} className="px-4 border-box my-2">{
+                                                (()=>{
+                                                    switch(e.dia){
+                                                        case "0":
+                                                            return "domingo";
+                                                        case "1":
+                                                            return "segunda";
+                                                        case "2":
+                                                            return "terça";
+                                                        case "3":
+                                                            return "quarta";
+                                                        case "4":
+                                                            return "quinta";
+                                                        case "5":
+                                                            return "sexta";
+                                                        case "6":
+                                                            return "sexta";
+                                                        default:
+                                                            return "inválido";
+                                                    }
+                                                })()
+                                            }</div>
                                             <div className="my-2">{e.dtInicio}</div>
                                             <div className="my-2">{e.dtTermino}</div>
                                             <button className="bg-yellow-400">Editar</button>
-                                            <button className="bg-red-400">Apagar</button>
+                                            <button className="bg-red-400" onClick={() => apagarData(index)}>Apagar</button>
                                         </>
                                     ))}
                                 </div>
